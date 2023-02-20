bfad8690c04846b03e63bcfe98e09b8709fa3c03
Serina <serina.jefthas@gmail.com>
2023-02-20 15:21:00 -0800

    Updates

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
  * main - Entry point
- *
+ * Description: prints alphabet
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,9 +10,12 @@ int main(void)
 	char alphabetStr [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 	int i;
 
-	for (i = 0; i < strlen(alphabetStr); i++)
+	/*for (i = 0; i < strlen(alphabetStr); i++)
 	{
 		putchar(alphabetStr[i]);
-	}
+	}*/
+	for (char low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
 	return (0);
 }
