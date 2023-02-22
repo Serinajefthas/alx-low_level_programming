40938f53d37a14b646b3f8acae99daafc77484b4
Serina <serina.jefthas@gmail.com>
2023-02-21 17:13:49 -0800

    Add q6-11 ans files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add -adds two ints together and prints result
+ * @a: integer to add
+ * @b: other int to add
+ * Return: addition result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
