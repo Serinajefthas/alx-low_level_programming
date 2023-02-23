53e8171f0580dc0c986b89193626be69f35605a0
Serina <serina.jefthas@gmail.com>
2023-02-23 05:23:31 -0800

    Add q0-q2 ans of more nested loops&functions project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies 2 numbers tg
+ * @a: number 1 to mulitply
+ * @b: number 2 tto multiply w
+ * Return: product a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
