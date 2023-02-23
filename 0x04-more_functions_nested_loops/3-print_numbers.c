9341ebd2cdabc43789c2f97465cf94911ffc6319
Serina <serina.jefthas@gmail.com>
2023-02-23 10:23:17 -0800

    Add q3-q5

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints no.s 0-9
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
