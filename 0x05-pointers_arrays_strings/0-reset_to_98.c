83d5d253d126c40fc559b172803aa74187ab869f
Serina <serina.jefthas@gmail.com>
2023-02-27 14:21:10 -0800

    Pointers project q0,q1,q2 ans

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - modifies valueof *n is pointing to (ie valueof n)
+ * @n: pointer which points to a (stores its memory address)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns length of string in int
+ * @s: char string to test
+ * Return: int of the string length
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
