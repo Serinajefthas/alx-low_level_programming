76586d0731a518e099d84c2a5fb4cf197703587a
Serina <serina.jefthas@gmail.com>
2023-02-28 11:26:47 -0800

    Add q7, q8 ans

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints an array
+ * @a: array to print
+ * @n: number of elements in array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%d\n", a[i]);
+			break;
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies str that *src pointer points to,2 buffer *dest pointer points to
+ * @dest: pointer that points to str copied to
+ * @src: pointer which points to string to copy
+ * Return: dest pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+
