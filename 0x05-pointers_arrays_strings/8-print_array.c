c4e739da95333348416609ea1b5f01be2170c05d
Serina <serina.jefthas@gmail.com>
2023-02-28 14:20:38 -0800

    Update q8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,17 +12,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (n <= 0)
+		if (i == 0)
 			printf("%d", a[i]);
-
-		else if (i == (n - 1))
-		{
-			printf("%d\n", a[i]);
-			break;
-		}
 		else
-		{
-			printf("%d, ", a[i]);
-		}
+			printf(", %d", a[i]);
 	}
 }
