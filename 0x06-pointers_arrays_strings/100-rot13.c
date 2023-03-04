7ad6e4f996ed6500fb1ce725700a21bf4f0a15e1
Serina <serina.jefthas@gmail.com>
2023-03-03 16:58:21 -0800

    add q7-10 & update q8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,11 +10,12 @@ char *rot13(char *n)
 	int i, j;
 
 	char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	
 	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (j = 0; str1[i] != '\0'; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (n[i] == str1[j])
 			{
