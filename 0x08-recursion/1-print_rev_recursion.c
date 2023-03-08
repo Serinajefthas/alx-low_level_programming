bab1d5b89418ec9560fd5e7337df04e35c04ec71
Serina <serina.jefthas@gmail.com>
2023-03-08 10:24:47 -0800

    Add q1 ans

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,14 +7,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int i;
-
-	i = strlen(s);
-	if (i != 0)
+	if (*s)
 	{
-		_putchar(s[i]);
-		_print_rev_recursion(s - 1);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	else
-		_putchar('\n');
 }
