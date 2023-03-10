96f8578f8c0e90c2c17191e59955f47ea42192de
Serina <serina.jefthas@gmail.com>
2023-03-10 04:42:21 -0800

    Add First task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: argument count
+ * @argv: array of argument strings
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+	return (0);
+}
