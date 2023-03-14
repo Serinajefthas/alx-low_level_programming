1823387cc76463247d399e5743a2f34e1c45fc87
Serina <serina.jefthas@gmail.com>
2023-03-14 12:17:10 -0700

    Add task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - creates and initialises char array
+ * @size: size of array
+ * @c: char to initialise array w
+ * Return: NULL if sizze=0 or ptr to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+	char *arr = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - newly allocated space
+ * @str: pointer to existing str
+ * Return: pointer to newly allocated memory
+ */
+char *_strdup(char *str)
+{
+	int i;
+	
+	char *arr = malloc(sizeof(int) * strlen(str));
+	
+	if (arr == NULL)
+		return (NULL);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(str); i++)
+		arr[i] = str[i];
+
+	return (arr);
+}
