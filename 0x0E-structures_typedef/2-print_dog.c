5679177987a9df757073514ab2b33dfff9ab665a
Serina <serina.jefthas@gmail.com>
2023-03-22 12:59:12 -0700

    Add task 4 new dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,12 +7,11 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->age == 0)
-		d->age = atoi("(nil)");
-	if (d->owner == NULL)
-		d->owner = "(nil)";
-	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? (d->name) : "(nil)");
+		printf("Age: %.6f\n", (d->age) ? (d->age) : (0));
+		printf("Owner: %s\n", (d->owner) ? (d->owner) : "(nil)");
+		/*printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner); */	
+	}
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a pointer to a new dog in memory
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: var of type dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog1;
+
+	dog1 = malloc(sizeof(struct dog));
+
+	if (dog1 == NULL)
+		return (NULL);
+	dog1->name = name;
+	dog1->age = age;
+	dog1->owner = owner;
+	return (dog1);
+}
