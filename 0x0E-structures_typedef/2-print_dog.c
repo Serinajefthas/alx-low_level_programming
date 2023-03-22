f406cd06d28d457a5b874373a00d53a686ee0285
Serina <serina.jefthas@gmail.com>
2023-03-22 14:20:43 -0700

    Update task 2 betty, null cases task 4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,6 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", (d->name) ? (d->name) : "(nil)");
 		printf("Age: %.6f\n", (d->age) ? (d->age) : 0);
 		printf("Owner: %s\n", (d->owner) ? (d->owner) : "(nil)");
-		/*printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner); */	
+		/*printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner); */
 	}
 }
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1;
 
-	dog1 = malloc(sizeof(struct dog));
-
 	if (dog1 == NULL)
 		return (NULL);
+	dog1 = malloc(sizeof(struct dog));
+	dog1 = malloc(sizeof(struct dog));
 	dog1->name = name;
 	dog1->age = age;
 	dog1->owner = owner;
