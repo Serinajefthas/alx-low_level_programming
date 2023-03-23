858642d009263bf52a4a124b7d58cf3a545da178
Serina <serina.jefthas@gmail.com>
2023-03-23 09:40:51 -0700

    Updates to task 0, libraries added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "function_pointers.h"
-#include <stddef.h>
 /**
  * print_name - prints name
  * @name: name to print
- * @f: pointer to function f
+ * @f: pointer to function that prints string
  */
 void print_name(char *name, void (*f)(char *))
 {
