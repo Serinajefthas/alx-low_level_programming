8154e50714257443f1a69e17bee43ed2bbb7737a
Serina <serina.jefthas@gmail.com>
2023-04-03 03:53:35 -0700

    Add readme and task 0-print int linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - print elements of listint_t list
+ * @h: head pointer to list
+ * Return: number of nodes(in bytes)
+ */
+size_t print_listint(const listint_t *h)
+{
+	int num_nodes = 0;
+	const listint_t *current = NULL;
+
+	current = h;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		num_nodes++;
+	}
+	return (num_nodes);
+}
