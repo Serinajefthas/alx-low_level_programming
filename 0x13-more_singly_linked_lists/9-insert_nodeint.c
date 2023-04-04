1fcbc48977be70c2361e8f5e3371de1d81a07e10
Serina <serina.jefthas@gmail.com>
2023-04-04 13:47:55 -0700

    Update task 11 & 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,8 +19,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+	new->next = NULL;
 
-	while (curr != NULL)
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	while (curr != NULL && i < idx)
 	{
 		if (i == idx - 1)
 		{
