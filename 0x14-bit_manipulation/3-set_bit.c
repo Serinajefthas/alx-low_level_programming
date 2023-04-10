a4aa869b1ef4b012149342a583c0d858f085655c
Serina <serina.jefthas@gmail.com>
2023-04-10 13:52:49 -0700

    Update task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,10 +8,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	*n |= (1UL << index);
-	if (*n)
-		return (1);
-	else if (index >= sizeof(unsigned long int) * 8 || !n)
+	if (index > 63)
 		return (-1);
-	return (-1);
+	*n |= (1UL << index);
+	return (1);
 }
