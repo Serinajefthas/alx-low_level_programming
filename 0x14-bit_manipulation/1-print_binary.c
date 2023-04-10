0729cfc6e2cfa10870fab17ab376e48351611134
Serina <serina.jefthas@gmail.com>
2023-04-10 12:35:40 -0700

    Update task 1 to remove unecessary leading 0s

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,18 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
+	int i, leading_zero = 1;
 
 	for (i = sizeof(int) * 8 - 1; i >= 0; i--)
 	{
 		if (n & (1 << i))
+		{
 			printf("1");
-		else
+			leading_zero = 0;
+		}
+		else if (!leading_zero)
 			printf("0");
 	}
+	if (leading_zero)
+		printf("0");
 }
