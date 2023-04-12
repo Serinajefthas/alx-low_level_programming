12f151fafd2cf6b79b5fba2f19a7fe4485cc3267
Serina <serina.jefthas@gmail.com>
2023-04-12 14:28:38 -0700

    Update task 1-change method of creating file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,5 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 /**
  * create_file - creates a file
  * @filename: file ptr
@@ -12,30 +8,18 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fp;
-	mode_t permissions = S_IRUSR | S_IWUSR;
+	int fd, w;
 
 	if (!filename)
 		return (-1);
-	chmod(filename, permissions);
-	/* try to append to existing file*/
-	fp = fopen(filename, "a");
-	if (!fp)
-		return (-1);
 	if (text_content == NULL)
-		text_content = " ";
-	if (fp != NULL)
-	{
-		fputs(text_content, fp);
-		fclose(fp);
-		return (1);
-	}
-	/* if file doesn't exist, try create one*/
-	fp = fopen(filename, "w");
-	if (!fp)
-		return (-1);
+		text_content = "";
 
-	fputs(text_content, fp);
-	fclose(fp);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, strlen(text_content));
+
+	if (fd == -1 || w == -1)
+		return (-1);
+	close(fd);
 	return (1);
 }
