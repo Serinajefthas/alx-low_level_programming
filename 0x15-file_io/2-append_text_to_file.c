2984124678d40f1913ede42c226afe79e2e51ccc
Serina <serina.jefthas@gmail.com>
2023-04-13 00:55:04 -0700

    Update task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,8 +10,10 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, w;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = '';
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
