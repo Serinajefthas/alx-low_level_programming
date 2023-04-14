b0e3446067280aaaa0b6b134acdd8e326cbe1b88
Serina <serina.jefthas@gmail.com>
2023-04-14 04:08:37 -0700

    Update task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 	r = read(src_fd, buffer, 1024);
 	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1])
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	w = write(dest_fd, buffer, 1024);
