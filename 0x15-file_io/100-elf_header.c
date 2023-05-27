f4b319e2da1841e425d71f1d5249890994fa4114
Serinajefthas <serina.jefthas@gmail.com>
2023-05-27 06:16:04 -0700

    Task 4 updates

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,33 +1,81 @@
 #include "main.h"
-#include <elf.h>
 
-void print_info(Elf64_Ehdr *header)
+void _magic(unsigned char *e_ident)
 {
-        long unsigned int i;
+	int i;
 
-        printf("ELF Magic Number: ");
-        for (i = 0; i < sizeof(header->e_ident); ++i)
-                printf("%02x ", header->e_ident[i]);
-        printf("\n");
-        printf("Class: %d\n", header->e_ident[EI_CLASS]);
-        printf("Data: %d\n", header->e_ident[EI_DATA]);
-        printf("Version: %d\n", header->e_ident[EI_VERSION]);
-        printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
-        printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-        printf("Type: %d\n", header->e_type);
-        printf("Machine: %d\n", header->e_machine);
-        printf("Version: %d\n", header->e_version);
-        printf("Entry point: 0x%lx\n", header->e_entry);
-        printf("Program header offset: 0x%lx\n", header->e_phoff);
-        printf("Section header offset: 0x%lx\n", header->e_shoff);
-        printf("Flags: 0x%x\n", header->e_flags);
-        printf("Size of this header: %d\n", header->e_ehsize);
-        printf("Size of program headers: %d\n", header->e_phentsize);
-        printf("Number of program headers: %d\n", header->e_phnum);
-        printf("Size of section headers: %d\n", header->e_shentsize);
-        printf("Number of section headers: %d\n", header->e_shnum);
-        printf("Section header string table index: %d\n", header->e_shstrndx);
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", e_ident[i]);
+	}
+	printf("\n");
+	return;
 }
+void _class(unsigned char *e_ident)
+{
+	printf("  Class:                             ");
+	printf("%x\n", e_ident[EI_DATA]);
+	if (e_ident[EI_CLASS] == ELFCLASSNONE)
+		printf("none\n");
+	if (e_ident[EI_CLASS] == ELFCLASS32)
+		printf("ELF32\n");
+	if (e_ident[EI_CLASS] == ELFCLASS64)
+		printf("ELF64\n");
+	else
+		printf("Unknown %x\n", e_ident[EI_CLASS]);
+	
+	printf("  Data:                              ");
+	if (e_ident[EI_DATA] == ELFDATANONE)
+		printf("none\n");
+	if (e_ident[EI_DATA] == ELFDATA2LSB)
+		printf("2's complement, little endian\n");
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+		printf("2's complement, big endian\n");
+	else
+		printf("Unknown %x\n", e_ident[EI_CLASS]);
+}
+
+
+
+/*void print_info(Elf64_Ehdr *header)
+{
+     	printf("ELF Header:\n");
+	_magic(header->e_ident);
+	_class(header);
+	return;
+}*/
+
+/**
+ * read_file - reads file
+ * @header: header
+ * @file: file
+ */
+void read_file(Elf64_Ehdr *header, char *file)
+{
+       	int o, r, c;
+
+        o = open(file, O_RDONLY);
+        if (o == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't read file %s\n", file);
+                exit(98);
+        }
+        r = read(o, header, sizeof(Elf64_Ehdr));
+        if (r == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", file);
+                exit(98);
+        }
+        c = close(o);
+        if (c == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o);
+                exit(98);
+        }
+}
+
 
 /**
  * main - prints contents elf file
@@ -38,65 +86,22 @@ void print_info(Elf64_Ehdr *header)
 int main(int argc __attribute__((__unused__)), char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r, c;
 
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-		exit(98);
-	}
 	header = malloc(sizeof(Elf64_Ehdr));
-	if (!header)
+	/*for (i = 0; i < 4; i++)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-		exit(98);
-	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
-		exit(98);
-	}
-	c = close(o);
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d\n", o);
-		exit(98);
-	}
-	print_info(header);
-	return (0);
-	
-	
-	
-	/*Elf64_Ehdr *header;
-	long unsigned int i;
-
-	if (argc != 2)
-	{
-		printf("Incorrect number of args");
-		return 1;
-	}
-	i = fopen(argv[1], "r");
-	if (i == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-		exit(98);
-	}
-	i = fread(&header, sizeof(Elf64_Ehdr), 1, file);
-	if (i == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: %s: No such file\n", argv[1]);
-		exit(98);
-	}
-	print_info(header);
-	i = pclose(file);
-	if (i == -1)
-	{
-		dprintf(STDERR_FILENO,"Error: Can't close fd %s\n", argv[1]);
-		exit(98);
-	}
-	return (0);*/
+                if (header->e_ident[i] != 0x7F &&
+                    header->e_ident[i] != 'E' &&
+                    header->e_ident[i] != 'L' &&
+                    header->e_ident[i] != 'F')
+                {
+                        dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+                        exit(98);
+                }
+        }*/
+	read_file(header, argv[1]);
+	_magic(header->e_ident);
+	_class(header->e_ident);
+	close(open(argv[1], O_RDONLY));
+	return (0);	
 }
-
