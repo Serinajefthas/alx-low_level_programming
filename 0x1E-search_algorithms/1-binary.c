3096c89d8511c660eefe6688f1f4cb5af46dcebe
Serinajefthas <serina.jefthas@gmail.com>
2023-10-10 12:52:19 -0700

    Update task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -7,29 +7,39 @@
  * @value: value to search for
  * Return: index of value, else -1
  */
+#include "search_algos.h"
+
+/**
+ * binary_search - searches array using binary algo
+ * @array: pointer to first element in array
+ * @size: number elements in array
+ * @value: value to search for
+ * Return: index of value, else -1
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, mid, l, r;
+        size_t mid, l, r, i;
 
-	l = mid = array[size -1];
-	r = array[0];
-	while (l <= r)
-	{
-		printf("Searching in array: ");
-		for (i = 0; i <= mid; i++)
-		{
-			if (i == mid)
-				printf("%d", array[i]);
-			else
-				printf("%d, ", array[i]);
-		}
-		mid = l + (r -1) / 2;
-		if (array[mid] == value)
-			return (mid);
-		if (array[mid] < value)
-			l = mid + 1;
-		else
-			r = mid - 1;
-	}
-	return (-1);
+        /* r, l, mid are all array indices, not array values*/
+        l = 0;
+        r = size - 1;
+        while (l <= r)
+        {
+                mid = l + (r - l) / 2;
+                printf("Searching in array: ");
+                for (i = 0; i <= r; i++)
+                {
+                        if (i == r)
+                                printf("%d\n", array[i]);
+                        else
+                                printf("%d, ", array[i]);
+                }
+                if (value == array[mid])
+                        return (mid);
+                else if (value > array[mid])
+                        l = mid + 1;
+                else
+                        r = mid - 1;
+        }
+        return (-1);
 }
